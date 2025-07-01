24d2dfda7a1e5c4895f783066de692787f20bee3
Nichi <rafaonishi99@gmail.com>
2025-07-01 15:02:13 -0300

    Add files via upload

---
+++ tiposdedados.py
@@ -0,0 +1,23 @@
+cachorro = 'cokkie' #criei uma variavel chamada cachorro, e armazenei a string cokkie
+celular = 19992871148
+preco_minecraft = 59.90
+github_nichi = 'nichi739'
+
+idade_onichi = 11
+idade_tio_alvoro = 11
+
+
+#usando o método type() para indentificar o tipo de dado cachorro
+print(type(cachorro))
+
+#usando o método type() para indentificar o tipo de dado celular
+print(type(celular))
+
+#usando o método type() para indentificar o tipo de dado preco minecraft
+print(type(preco_minecraft))
+
+#usando o método type() para indentificar o tipo de dado github
+print(type(github_nichi))
+
+#usando o método type() para indentificar se algo é verdadeiro ou não
+print(type(idade_onichi > idade_tio_alvoro))
