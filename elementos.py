b3fe3ed49012e64832ce452fa64eb208a248e62f
Nichi <rafaonishi99@gmail.com>
2025-07-02 15:12:50 -0300

    Add files via upload

---
+++ elementos.py
@@ -0,0 +1,32 @@
+#Criando uma variável
+idade_onishi = 11
+idade_alvaro = 33
+
+idade_paulo = input("Digite a idade de Paulo: ")
+
+print(idade_paulo)
+
+#Operadores
+primeiro_numero = int(input("Digite o primeiro numero"))
+segundo_numero = int(input("Digite o segundo numero"))
+
+#SOMA
+soma = primeiro_numero + segundo_numero
+print (f'a soma dos numeros é de {soma}')
+
+#sobtração
+
+subtração = primeiro_numero - segundo_numero
+print (f'a subtração dos numeros é de {subtração}')
+#multiplicão
+multiplicação = primeiro_numero * segundo_numero
+print (f'a multiplicação dos numeros é de {multiplicação}')
+#divisão
+divisão = primeiro_numero / segundo_numero
+print (f'a divisão dos numeros é de {divisão}')
+
+
+
+
+
+
