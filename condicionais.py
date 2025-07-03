d3cfd9395984928c80e8279af5b20f48cc32a5ba
Nichi <rafaonishi99@gmail.com>
2025-07-03 14:25:00 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,7 @@
+#CRIANDO UMA ESTRUTURA DE FLUXO PARA VERIFICAR SE EU POSSO DIRIGIR OU NÃO
+idade = int(input("Qual a sua idade?"))
+
+if idade >= 18:
+    print("Parabéns, você pode dirigir!")
+else:
+    print("Você não pode dirigir")
