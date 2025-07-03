c7308fb51021dea6806d19d85b00496b70d2cac0
Nichi <rafaonishi99@gmail.com>
2025-07-03 14:50:20 -0300

    Add files via upload

---
+++ lacos.py
@@ -0,0 +1,5 @@
+#CRIANDO UMA ESTRUTURA DE LAÇO PARA IMPRIMIR NÚMEROS DE 1 A 500
+
+
+for n in range(1000): #para cada número no intervalo de 0 a 500...
+    print(f'O numéro é: {n}') #printe até 500
