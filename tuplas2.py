fead7022c07d5de8b06519aec8ac04adc40257e5
Nichi <rafaonishi99@gmail.com>
2025-08-07 16:40:13 -0300

    Add files via upload

---
+++ tuplas2.py
@@ -0,0 +1,12 @@
+nomes = ("alvaro",
+         'letIcia',
+         'bruno',
+         'henrique',
+         'Carlos')
+
+
+print(nomes[1:4])      #nomedatupla[INICIO:final+1 PASSO]
+print(nomes[0:3]) #('alvaro',leticia bruno)
+print(nomes[2::])# (bruno leticia carlos)
+print(nomes[:: 2]) ()
+print(nomes[:: -1])
