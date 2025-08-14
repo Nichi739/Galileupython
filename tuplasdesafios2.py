97628bd4fa79577a884ea84e796dbfa384f3eac1
Nichi <rafaonishi99@gmail.com>
2025-08-14 17:04:22 -0300

    Add files via upload

--- tuplasdesafios2.py
+++ tuplasdesafios2.py
@@ -8,6 +8,7 @@ pedidos = [
     ("Jaqueta", "Vestuário", 139.90, "Cancelado"),
 ]
 
+somacanceladovestuario = 0
 
 pedidos_entregues = []
 print('PRODUTOS ENTREGUES'.center(40, '-'))
@@ -18,3 +19,21 @@ for i in pedidos:
 print(pedidos_entregues)
 soma = sum(pedidos_entregues)
 print (soma)
+
+print('Ultimos 3 pedidos:')
+
+
+print(pedidos[-1])
+
+for i in pedidos:
+    if i[3] == 'Cancelado' and i[1] == 'Vestuário':
+        somacanceladovestuario += 1
+print(f'cancelados da categoria vestuario: {somacanceladovestuario}')
+
+filtro = input('Digite uma categoria para filtrar: ')
+
+for i in pedidos:
+    if filtro == i[1]:
+        print(f'- {i[0]}')
+
+
