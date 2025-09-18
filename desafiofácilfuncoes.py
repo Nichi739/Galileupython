3e07f561c811cd5097a3a4d2741009001602f26c
Nichi <rafaonishi99@gmail.com>
2025-09-18 17:32:45 -0300

    Add files via upload

---
+++ desafiofácilfuncoes.py
@@ -0,0 +1,16 @@
+listas_notas = [10,7,8]
+
+def calcular_media(listas_notas):
+ 
+ media =  sum(listas_notas)/len(listas_notas)
+ return print(f'A media de notas é de: {media}')
+
+calcular_media(listas_notas)
+
+
+
+
+    
+
+
+
