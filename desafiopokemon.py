bfcf268ffb764df5d5378d095772beff30be4bef
Nichi <rafaonishi99@gmail.com>
2025-10-23 17:04:17 -0300

    Add files via upload

--- desafiopokemon.py
+++ desafiopokemon.py
@@ -2,7 +2,7 @@
 import random
 
 pokemons = {
-    'charmander': {'ataque':'bola de fogo','dano':3500},
+    'charmander': {'ataque':'bola de fogo','dano':35},
     'squirtlechatao': {'ataque':'jato de água','dano': 32},
     'bulbassauro': {'ataque':'chicote de sipó','dano': 30},
 }
@@ -43,7 +43,21 @@ ficha_habilidades(escolha2)
 def dano_critico (escolha):
     dano_critico = pokemons[escolha]['dano'] * random.uniform(1,2)
     print(f'o dano_critico do {escolha} é de {dano_critico:.2f}')
+    return dano_critico
 
-dano_critico(escolha1)
-dano_critico(escolha2)
+d1 =dano_critico(escolha1)
+d2 = dano_critico(escolha2)
 
+#CRIEM UMA FUNCAO SE CHAMADA BATALHA (DANO) (DANOCRITICO1) (DANOCRITICO2)
+#CADA POKEMON TEM 100 DE VIDa
+#NO FINAL DESSA BATALHA, MOSTRE O VENCEDOR
+
+def batalha (dano1, dano2):
+    vida1 = 100 - dano2
+    vida2 = 100 - dano1
+    if vida1 > vida2:
+        print(f'o vencedor é o player1, vida restante: {vida1:.2f}')
+    else:
+        print(f'o vencedor é o player2, vida restante {vida2:.2f}')
+
+batalha(d1,d2)
